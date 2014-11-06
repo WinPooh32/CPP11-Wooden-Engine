109bc600b485b53ec75f16b9178b0ed046bc7a4a
WinPooh32 <deathkill2d@gmail.com>
2014-11-06 16:27:54 +1100

    Widget class update

--- src/Widget.cpp
+++ src/Widget.cpp
@@ -6,13 +6,59 @@
  */
 
 #include "Widget.h"
+#include <iostream>
 
-Widget::Widget() {
-	// TODO Auto-generated constructor stub
-
+Widget::Widget(Widget* parent = 0) {
+	_parent = parent;
+	_rect = {0, 0, 32, 32};
 }
 
 Widget::~Widget() {
-	// TODO Auto-generated destructor stub
+	ChildrenList.clear();
+	//TODO say parent that u are dead
 }
 
+void Widget::Move(const int& x, const int& y){
+	_rect.x = x;
+	_rect.y = y;
+}
+
+void Widget::SetWidth(const int& width){
+	_rect.w = width;
+}
+
+void Widget::SetHeight(const int& height){
+	_rect.h = height;
+}
+
+void Widget::OnEvent(SDL_Event* event){
+
+}
+
+void Widget::OnUpadate(){
+
+}
+
+void Widget::OnRender(){
+
+}
+
+void Widget::AddChild(Widget* child){
+	for(auto it = ChildrenList.begin(); it != ChildrenList.end(); it++){
+		if(*it == child){
+			std::cout << "Widget::AddChild() DEBUG: child is existed." << std::endl;
+			return;
+		}
+	}
+	ChildrenList.push_front(child);
+}
+
+void Widget::RemoveChild(Widget* child){
+	for(auto it = ChildrenList.begin(); it != ChildrenList.end(); it++){
+		if(*it == child){
+			std::cout << "Widget::AddChild() DEBUG: child was deleted." << std::endl;
+			ChildrenList.erase(it);
+			return;
+		}
+	}
+}
--- src/Widget.h
+++ src/Widget.h
@@ -8,17 +8,32 @@
 #ifndef WIDGET_H_
 #define WIDGET_H_
 
+#include <list>
 #include <SDL2/SDL.h>
 
 class Widget {
 public:
-	Widget();
+	Widget(Widget* parent);
 	virtual ~Widget();
 
-	void Move(int x, int y);
+	void Move(const int& x, const int& y);
+	void SetWidth(const int& width);
+	void SetHeight(const int& height);
+
+public:
+	virtual void OnEvent(SDL_Event* event);
+	virtual void OnUpadate();
+	virtual void OnRender();
 
 protected:
-	SDL_Rect rect;
+	SDL_Rect _rect;
+
+	void AddChild(Widget* child);
+	void RemoveChild(Widget* child);
+
+private:
+	Widget* _parent;
+	std::list<Widget*> ChildrenList;
 };
 
 #endif /* WIDGET_H_ */
