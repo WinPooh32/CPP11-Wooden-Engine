3e9f01a43a839b788f66eccdcccc64953efcb8ae
WinPooh32 <deathkill2d@gmail.com>
2014-11-08 19:10:35 +1000

    Widget update

--- src/Widget.cpp
+++ src/Widget.cpp
@@ -7,67 +7,133 @@
 
 #include "Widget.h"
 
-Widget::Widget(Widget* parent = 0) {
-	_parent = parent;
-	_rect = {0, 0, 32, 32};
+Widget::Widget(Widget* parent, int x, int y, int w, int h) {
+	SetParent(parent);
+	_rect = {x, y, w, h};
+	SetVisible(true);
+	std::cout << "OnWidgetCreate\n";
 }
 
 Widget::~Widget() {
 	//Delete all chindren
-	for(auto it = ChildrenList.begin(); it != ChildrenList.end(); it++){
-		if(*it != nullptr){
+	for (auto it = ChildrenList.begin(); it != ChildrenList.end(); it++) {
+		if (*it != nullptr) {
 			delete *it;
 		}
 	}
 	ChildrenList.clear();
 
 	//Say parent to remove this child
-	if(_parent != nullptr){
+	if (_parent != nullptr) {
 		_parent->RemoveChild(this);
 	}
 }
 
-void Widget::Move(const int& x, const int& y){
+void Widget::Move(const int& x, const int& y) {
 	_rect.x = x;
 	_rect.y = y;
 }
 
-void Widget::SetWidth(const int& width){
+void Widget::SetParent(Widget* parent) {
+
+	_parent = parent;
+
+	if(_parent != nullptr){
+		_parent->AddChild(this);
+	}
+
+}
+
+void Widget::SetWidth(const int& width) {
 	_rect.w = width;
 }
 
-void Widget::SetHeight(const int& height){
+void Widget::SetHeight(const int& height) {
 	_rect.h = height;
 }
 
-void Widget::OnEvent(SDL_Event* event){
+void Widget::SetVisible(const bool& visible) {
+	_visible = visible;
+}
+
+Widget* Widget::GetParent() {
+	return _parent;
+}
+
+const SDL_Rect& Widget::GetRect() {
+	return _rect;
+}
+
+const bool& Widget::isVisible() {
+	return _visible;
+}
+
+void Widget::OnEvent(SDL_Event* event) {
 
 }
 
-void Widget::OnUpadate(){
+void Widget::OnUpdate() {
+	//do nothing
+}
+
+void Widget::OnRender() {
+	//do nothing
+}
+
+void Widget::OnUpdateChildren() {
+
+	if (ChildrenList.empty()) {
+		return;
+	}
+
+	for (auto it = ChildrenList.begin(); it != ChildrenList.end(); it++) {
+		(*it)->OnUpdate();
+		(*it)->OnUpdateChildren();
+	}
 
 }
 
-void Widget::OnRender(){
+void Widget::OnRenderChildren() {
+
+	if (ChildrenList.empty() || !_visible) {
+		return;
+	}
+
+	for (auto it = ChildrenList.begin(); it != ChildrenList.end(); it++) {
+		(*it)->OnRender();
+		(*it)->OnRenderChildren();
+	}
 
 }
 
-void Widget::AddChild(Widget* child){
-	for(auto it = ChildrenList.begin(); it != ChildrenList.end(); it++){
-		if(*it == child){
-			std::cout << "Widget::AddChild() DEBUG: child is existed." << std::endl; //TODO Is checking for child existance needed?
-			return;
+void Widget::AddChild(Widget* child) {
+
+	if (!ChildrenList.empty()) {
+		for (auto it = ChildrenList.begin(); it != ChildrenList.end(); it++) {
+			if (*it == child) {
+				std::cout << "Widget::AddChild() DEBUG: child is existed."
+						<< std::endl; //TODO Is checking for child existance needed?
+				return;
+			}
 		}
 	}
+
 	ChildrenList.push_front(child);
+
 }
 
-void Widget::RemoveChild(Widget* child){
-	for(auto it = ChildrenList.begin(); it != ChildrenList.end(); it++){
-		if(*it == child){
-			std::cout << "Widget::AddChild() DEBUG: child was deleted." << std::endl;
+void Widget::RemoveChild(Widget* child) {
+	if (ChildrenList.empty()) {
+		return;
+	}
+
+	for (auto it = ChildrenList.begin(); it != ChildrenList.end(); it++) {
+		if (*it == child) {
+			std::cout << "Widget::AddChild() DEBUG: child was deleted."
+					<< std::endl;
 			ChildrenList.erase(it);
 			return;
 		}
 	}
+
 }
--- src/Widget.h
+++ src/Widget.h
@@ -15,22 +15,34 @@
 
 class Widget {
 public:
-	Widget(Widget* parent);
+	Widget(Widget* parent = nullptr, int x = 0, int y = 0, int w = 0,
+			int h = 0);
 	virtual ~Widget();
 
 	void Move(const int& x, const int& y);
+
+	void SetParent(Widget* parent);
 	void SetWidth(const int& width);
 	void SetHeight(const int& height);
+	void SetVisible(const bool& visible);
 
-	virtual void OnEvent(SDL_Event* event);
-	virtual void OnUpadate();
+	Widget* GetParent();
+	const SDL_Rect& GetRect();
+	const bool& isVisible();
+
+	virtual void OnEvent(SDL_Event* event);//TODO implement
+	virtual void OnUpdate();
 	virtual void OnRender();
+
+	void OnUpdateChildren();
+	void OnRenderChildren();
 
 protected:
 	SDL_Rect _rect;
 
 private:
 	Widget* _parent;
+	bool _visible;
 	std::list<Widget*> ChildrenList;
 
 	void AddChild(Widget* child);
