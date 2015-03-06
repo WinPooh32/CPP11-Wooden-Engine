e4d556aeabe527d61ae9c7130cf754df5ac48017
WinPooh32 <deathkill2d@gmail.com>
2015-03-06 19:23:51 +1000

    Inplemented simple sprite animation

--- src/Render/Sprite.cpp
+++ src/Render/Sprite.cpp
@@ -8,28 +8,46 @@
 #include <Render/Sprite.h>
 
 Sprite::Sprite() {
-	_texture = nullptr;
+    _texture = nullptr;
+    _isAnimated = false;
 }
 
 Sprite::~Sprite() {
 
 }
 
-void Sprite::Draw(){
-	//TODO Make animation
-	Surface::OnDraw(_texture, &_rect);
+void Sprite::Draw() {
+    if (_isAnimated) {
+        SDL_Rect anim_rect = {_rect.w * _anim_control.GetCurrentFrame(),0 ,_rect.w, _rect.h};
+        Surface::OnDraw(_texture, &anim_rect, &_rect);
+        _anim_control.OnAnimation();
+    } else {
+        Surface::OnDraw(_texture, &_rect);
+    }
 }
 
-void Sprite::SetPos(const Vec2& new_pos){
-	_rect.x = new_pos.x;
-	_rect.y = new_pos.y;
+void Sprite::SetPos(const Vec2& new_pos) {
+    _rect.x = new_pos.x;
+    _rect.y = new_pos.y;
 }
 
-void Sprite::SetSize(const Vec2& size){
-	_rect.w = size.x;
-	_rect.h = size.y;
+void Sprite::SetSize(const Vec2& size) {
+    _rect.w = size.x;
+    _rect.h = size.y;
 }
 
-void Sprite::SetTexture(SDL_Texture* texture){
-	_texture = texture;
+void Sprite::SetTexture(SDL_Texture* texture) {
+    _texture = texture;
+
+    //Get texture size
+    if (_texture) {
+        SDL_QueryTexture(_texture, nullptr, nullptr, &_src_rect.w,
+                &_src_rect.h);
+    }
+}
+
+void Sprite::SetAnimation(int begin_frame, int end_frame) {
+    _isAnimated = true;
+    _anim_control.SetBeginFrame(begin_frame);
+    _anim_control.SetMaxFrame(end_frame);
 }
--- src/Render/Sprite.h
+++ src/Render/Sprite.h
@@ -9,21 +9,25 @@
 #define SRC_RENDER_SPRITE_H_
 
 #include <Render/Surface.h>
+#include <Render/Animation.h>
 
 class Sprite {
 public:
-	Sprite();
-	virtual ~Sprite();
+    Sprite();
+    virtual ~Sprite();
 
-	void Draw();
-	void SetPos(const Vec2& new_pos);
-	void SetSize(const Vec2& size);
-
-	void SetTexture(SDL_Texture* texture);
+    void Draw();
+    void SetPos(const Vec2& new_pos);
+    void SetSize(const Vec2& size);
+    void SetTexture(SDL_Texture* texture);
+    void SetAnimation(int begin_frame, int end_frame);
 
 private:
-	SDL_Rect _rect;
-	SDL_Texture* _texture;
+    bool _isAnimated;
+    SDL_Rect _rect;
+    SDL_Rect _src_rect;//real size of texture
+    SDL_Texture* _texture;
+    Animation _anim_control;
 };
 
 #endif /* SRC_RENDER_SPRITE_H_ */
