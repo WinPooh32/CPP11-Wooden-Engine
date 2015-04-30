0eff3af65b7fe5c08a708c071e2b9be483e8f824
Christian Smithroat <cjsmithro@gmail.com>
2015-04-30 13:09:20 -0400

    reformatted level editor

--- Map_Demo/LevelEditorSave/gui.cpp
+++ Map_Demo/LevelEditorSave/gui.cpp
@@ -19,90 +19,73 @@ int main()
 	
 
 	for(int i=0; i < 13 ;i++)
-			{
-			  for(int j=0;j < 17 ;j++)
-				    {
-					if ( i == 0 && j < 16)
-
-							{
-							//cout<<setw(2)<<setfill('-')<<j<<" ";	
-							}
-						
-							else if(i <12&& j==0 )
-							{
-							//cout<<setw(2)<<setfill('-')<< i <<" ";
-							}	
-						else
-							{	
-								array1[i][j]==0;
-								array1[i][j]=num;
-								//cout <<setw(2)<<setfill('0')<<array1[i][j] << " " ;
-							}			
-					}
-				//cout<<endl;
-			}
+	{
+		for(int j=0;j < 17 ;j++)
+		{
+			num==0;
+			array1[i][j]=num;			
+		}	
+	}
 
 
 	while(edMode ==true)
 	{
 		for(int i=0; i < 13 ;i++)
 		{
-		  for(int j=0;j < 17 ;j++)
-			    {
-				if ( i == 0 && j < 16)
-
-						{
-						cout<<setw(2)<<setfill('-')<<j<<" ";	
-						}
-					
-						else if(i <12&& j==0 )
-						{
-						cout<<setw(2)<<setfill('-')<< i <<" ";
-						}	
-					else
-						{	
-							
-						cout <<setw(2)<<setfill('0')<<array1[i][j] << " " ;
-						}			
+			for(int j=0;j < 17 ;j++)
+			{
+				if ( i == 0 && j < 17)
+				{
+					cout<<setw(2)<<setfill('-')<<j<<" ";	
 				}
+					
+				else if(i <13&& j==0 )
+				{
+					cout<<setw(2)<<setfill('-')<< i <<" ";
+				}	
+				else
+				{	
+							
+					cout <<setw(2)<<setfill('0')<<array1[i][j] << " " ;
+				}			
+			}
+
 			cout<<endl;
 		}
 
-	cout<<"Close|100| Save|101| Load|102|"<< endl <<"Put the y of the box you want to change:";
-	cin>>x;
-	if(x == 100)
-	{
-		edMode == true; 
-		return 0;
-	}
-	if(x == 101)
-	{
-		ofstream out;
-		out.open("savedate.txt");
-		for(int i=1; i < 13 ;i++)
+		cout<<"Close|100| Save|101| Load|102|"<< endl <<"Type number of the row of the tile you want to change:";
+		cin>>x;
+
+		if(x == 100)
 		{
-		for(int j=1;j< 17 ;j++)
-	    {
-			
-			num==00;
-			array1[i][j]=num;
-			//out <<setw(2)<<setfill('0')<<array1[i][j] << " " ;
-			
+			edMode == true; 
+			return 0;
 		}
-		out<< "File Saved.";
-	}
-	out.close();
-	}
-	if(x == 102)
-	{
-	ifstream myFile("chaaou.txt");
-	int i = 1;
-	int j = 1;
+		if(x == 101)
+		{
+			ofstream out;
+			out.open("savedate.txt");
+			for(int i=1; i < 13 ;i++)
+			{
+				for(int j=1;j< 17 ;j++)
+	   			 {
+					num==00;
+					array1[i][j]=num;
+			
+				 }
+
+			       	out<< "File Saved.";
+			}
+			out.close();
+		}
+		if(x == 102)
+		{
+			ifstream myFile("chaaou.txt");
+			int i = 1;
+			int j = 1;
 	
 				while(myFile >> array1[i][j])
-				{
-					//cout << std::setw(2) << std::setfill('0') << array1[i][j] << " " ;
-					
+				{	
 					if(i == 16 && j == 12)
 					{
 						return 0;
@@ -119,45 +102,16 @@ int main()
 					}
 				}
 		
-	}
+		}
 	
-	if(x < 100)
-	{
-	cout<<"put the x of the one you want to change"<<endl;
-        cin>>y;
-	cout<<"put the number you want to put"<<endl;
-        cin>>output;
-	array1[x][y]=output;
-	cout<<x<<y<<output<<endl;
-	}
+		if(x < 100)
+		{
+			cout<<"Type the number of the column of the tile you want to change:"<<endl;
+        		cin>>y;
+			cout<<"Type the number of the tile you want to select:"<<endl;
+        		cin>>output;
+			array1[x][y]=output;
+		}
 
 	}
 }
-
-
-
-
-// void save()
-// {
-// 	int num2;
-// int array2[16][12];
-// ofstream out;
-// out.open("savedate.txt");
-// for(int i=0; i < 12 ;i++)
-// 	{
-// 	  for(int j=0;j< 16 ;j++)
-// 	    {
-			
-			
-// 			array2[i][j]=num2;
-// 			 out <<setw(2)<<setfill('0')<<array1[i][j] << " " ;
-			
-// 		}
-// 		out<<endl;
-// 	}
-// 	out.close();
-// }
-
-
-	
-
