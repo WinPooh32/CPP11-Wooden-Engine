8460454b2fb7402063fab77692a4a31c90090030
ou zheng <ozheng@stetson.edu>
2015-04-29 20:53:59 -0400

    save with 2d array

---
+++ save2.cpp
@@ -0,0 +1,30 @@
+    #include <iostream>
+    #include <fstream>
+    #include <string>
+    #include <cstdlib>
+	 using namespace std;
+	//save
+	 #undef main
+
+int num;
+int array1[16][12];
+
+int main()
+{
+ofstream out;
+out.open("savedate.txt");
+for(int i=0; i < 12 ;i++)
+	{
+	  for(int j=0;j< 16 ;j++)
+	    {
+			
+			num==0;
+			array1[i][j]=num;
+			out << array1[i][j] << " " ;
+			
+		}
+		out<<endl;
+	}
+	out.close();
+}
+
